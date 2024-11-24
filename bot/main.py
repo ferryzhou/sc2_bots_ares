f5d25ca1ea1b5162153913794c03df399f9c9d83
Jin Zhou <ferryzhou@gmail.com>
2024-11-23 16:37:00 -0800

    Enable attacking and micro. Mostly copied from https://github.com/AresSC2/ares-random-example/blob/main/bot/main.py.

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,40 @@
+from itertools import cycle
 from typing import Optional
 
+import numpy as np
+
 from ares import AresBot
+from ares.behaviors.combat import CombatManeuver
+from ares.behaviors.combat.individual import (
+    AMove,
+    KeepUnitSafe,
+    PathUnitToTarget,
+    ShootTargetInRange,
+    StutterUnitBack,
+    UseAbility,
+)
 from ares.behaviors.macro import AutoSupply, Mining, SpawnController, ProductionController
 from sc2.data import Race
 from sc2.ids.unit_typeid import UnitTypeId as UnitID
+from sc2.position import Point2
+from ares.consts import ALL_STRUCTURES, WORKER_TYPES, UnitRole, UnitTreeQueryType
+from cython_extensions import cy_closest_to, cy_in_attack_range, cy_pick_enemy_target
+from sc2.data import Race
+from sc2.ids.ability_id import AbilityId
+from sc2.ids.unit_typeid import UnitTypeId as UnitID
+from sc2.ids.upgrade_id import UpgradeId
+from sc2.position import Point2
+from sc2.unit import Unit
+from sc2.units import Units
 
+COMMON_UNIT_IGNORE_TYPES: set[UnitID] = {
+    UnitID.EGG,
+    UnitID.LARVA,
+    UnitID.CREEPTUMORBURROWED,
+    UnitID.CREEPTUMORQUEEN,
+    UnitID.CREEPTUMOR,
+    UnitID.MULE,
+}
 # this will be used for ares SpawnController behavior
 ARMY_COMPS: dict[Race, dict] = {
     Race.Protoss: {
@@ -29,7 +59,12 @@ ARMY_COMPS: dict[Race, dict] = {
     # },
 }
 
+# Mostly copied from https://github.com/AresSC2/ares-random-example/blob/main/bot/main.py
 class MyBot(AresBot):
+    expansions_generator: cycle
+    current_base_target: Point2
+    _begin_attack_at_supply: float
+
     def __init__(self, game_step_override: Optional[int] = None):
         """Initiate custom bot
 
@@ -41,12 +76,68 @@ class MyBot(AresBot):
         """
         super().__init__(game_step_override)
 
+        self._commenced_attack: bool = False
+
+    @property
+    def attack_target(self) -> Point2:
+        if self.enemy_structures:
+            # using a faster cython alternative here, see docs for all available functions
+            # https://aressc2.github.io/ares-sc2/api_reference/cython_extensions/index.html
+            return cy_closest_to(self.start_location, self.enemy_structures).position
+        # not seen anything in early game, just head to enemy spawn
+        elif self.time < 240.0:
+            return self.enemy_start_locations[0]
+        # else search the map
+        else:
+            # cycle through expansion locations
+            if self.is_visible(self.current_base_target):
+                self.current_base_target = next(self.expansions_generator)
+
+            return self.current_base_target
+
+    async def on_start(self) -> None:
+        """
+        Can use burnysc2 hooks as usual, just add a call to the
+        parent method before your own logic.
+        """
+        await super(MyBot, self).on_start()
+
+        self.current_base_target = self.enemy_start_locations[0]
+        self.expansions_generator = cycle(
+            [pos for pos in self.expansion_locations_list]
+        )
+        self._begin_attack_at_supply = 3.0 if self.race == Race.Terran else 6.0
+
     async def on_step(self, iteration: int) -> None:
         await super(MyBot, self).on_step(iteration)
 
         self._macro()
 
-        pass
+        # https://aressc2.github.io/ares-sc2/api_reference/manager_mediator.html#ares.managers.manager_mediator.ManagerMediator.get_units_from_role
+        # see `self.on_unit_created` where we originally assigned units ATTACKING role
+        forces: Units = self.mediator.get_units_from_role(role=UnitRole.ATTACKING)
+
+        if self._commenced_attack:
+            self._micro(forces)
+        elif self.get_total_supply(forces) >= self._begin_attack_at_supply:
+            self._commenced_attack = True
+
+    async def on_unit_created(self, unit: Unit) -> None:
+        """
+        Can use burnysc2 hooks as usual, just add a call to the
+        parent method before your own logic.
+        """
+        await super(MyBot, self).on_unit_created(unit)
+
+        # assign our forces ATTACKING by default
+        if unit.type_id not in WORKER_TYPES and unit.type_id not in {
+            UnitID.QUEEN,
+            UnitID.MULE,
+            UnitID.OVERLORD,
+        }:
+            # here we are making a request to an ares manager via the mediator
+            # See https://aressc2.github.io/ares-sc2/api_reference/manager_mediator.html
+            self.mediator.assign_role(tag=unit.tag, role=UnitRole.ATTACKING)
 
     def _macro(self) -> None:
         # MINE
@@ -69,6 +160,94 @@ class MyBot(AresBot):
             ProductionController(ARMY_COMPS[self.race], self.start_location)
         )
         self.register_behavior(SpawnController(ARMY_COMPS[self.race]))
+
+    def _micro(self, forces: Units) -> None:
+        # make a fast batch distance query to enemy units for all our units
+        # key: unit tag, value: units in range of that unit tag
+        # https://aressc2.github.io/ares-sc2/api_reference/manager_mediator.html#ares.managers.manager_mediator.ManagerMediator.get_units_in_range
+        # as zerg we will only interact with ground enemy, else we should get all enemy
+        query_type: UnitTreeQueryType = (
+            UnitTreeQueryType.EnemyGround
+            if self.race == Race.Zerg
+            else UnitTreeQueryType.AllEnemy
+        )
+        near_enemy: dict[int, Units] = self.mediator.get_units_in_range(
+            start_points=forces,
+            distances=15,
+            query_tree=query_type,
+            return_as_dict=True,
+        )
+
+        # get a ground grid to path on, this already contains enemy influence
+        grid: np.ndarray = self.mediator.get_ground_grid
+
+        # make a single call to self.attack_target property
+        # otherwise it keep calculating for every unit
+        target: Point2 = self.attack_target
+
+        # use `ares-sc2` combat maneuver system
+        # https://aressc2.github.io/ares-sc2/api_reference/behaviors/combat_behaviors.html
+        for unit in forces:
+            """
+            Set up a new CombatManeuver, idea here is to orchestrate your micro
+            by stacking behaviors in order of priority. If a behavior executes
+            then all other behaviors will be ignored for this step.
+            """
+
+            attacking_maneuver: CombatManeuver = CombatManeuver()
+            # we already calculated close enemies, use unit tag to retrieve them
+            all_close: Units = near_enemy[unit.tag].filter(
+                lambda u: not u.is_memory and u.type_id not in COMMON_UNIT_IGNORE_TYPES
+            )
+            only_enemy_units: Units = all_close.filter(
+                lambda u: u.type_id not in ALL_STRUCTURES
+            )
+
+            if self.race == Race.Zerg:
+                # you can add a CombatManeuver to another CombatManeuver!!!
+                burrow_behavior: CombatManeuver = self.burrow_behavior(unit)
+                attacking_maneuver.add(burrow_behavior)
+
+            # enemy around, engagement control
+            if all_close:
+                # ares's cython version of `cy_in_attack_range` is approximately 4
+                # times speedup vs burnysc2's `all_close.in_attack_range_of`
+
+                # idea here is to attack anything in range if weapon is ready
+                # check for enemy units first
+                if in_attack_range := cy_in_attack_range(unit, only_enemy_units):
+                    # `ShootTargetInRange` will check weapon is ready
+                    # otherwise it will not execute
+                    attacking_maneuver.add(
+                        ShootTargetInRange(unit=unit, targets=in_attack_range)
+                    )
+                # then enemy structures
+                elif in_attack_range := cy_in_attack_range(unit, all_close):
+                    attacking_maneuver.add(
+                        ShootTargetInRange(unit=unit, targets=in_attack_range)
+                    )
+
+                enemy_target: Unit = cy_pick_enemy_target(all_close)
+
+                # low shield, keep protoss units safe
+                if self.race == Race.Protoss and unit.shield_percentage < 0.3:
+                    attacking_maneuver.add(KeepUnitSafe(unit=unit, grid=grid))
+
+                else:
+                    attacking_maneuver.add(
+                        StutterUnitBack(unit=unit, target=enemy_target, grid=grid)
+                    )
+
+            # no enemy around, path to the attack target
+            else:
+                attacking_maneuver.add(
+                    PathUnitToTarget(unit=unit, grid=grid, target=target)
+                )
+                attacking_maneuver.add(AMove(unit=unit, target=target))
+
+            # DON'T FORGET TO REGISTER OUR COMBAT MANEUVER!!
+            self.register_behavior(attacking_maneuver)
+
 
     """
     Can use `python-sc2` hooks as usual, but make a call the inherited method in the superclass
