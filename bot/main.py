2f64320f39487b3f72c5ccd81f3298947751a0dc
Jin Zhou <ferryzhou@gmail.com>
2024-11-23 16:10:40 -0800

    Enable auto supply and army building.

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,33 @@
 from typing import Optional
 
 from ares import AresBot
-from ares.behaviors.macro import AutoSupply, Mining, SpawnController
+from ares.behaviors.macro import AutoSupply, Mining, SpawnController, ProductionController
+from sc2.data import Race
+from sc2.ids.unit_typeid import UnitTypeId as UnitID
+
+# this will be used for ares SpawnController behavior
+ARMY_COMPS: dict[Race, dict] = {
+    Race.Protoss: {
+        UnitID.STALKER: {"proportion": 0.4, "priority": 0},
+        UnitID.ZEALOT: {"proportion": 0.3, "priority": 1},
+        UnitID.IMMORTAL: {"proportion": 0.15, "priority": 2},
+        UnitID.SENTRY: {"proportion": 0.1, "priority": 3},
+        UnitID.OBSERVER: {"proportion": 0.05, "priority": 4},
+    },
+    Race.Terran: {
+        UnitID.MARINE: {"proportion": 1.0, "priority": 0},
+    },
+    Race.Zerg: {
+        UnitID.ROACH: {"proportion": 1.0, "priority": 0},
+    },
+    # Example if using more than one unit
+    # proportion's add up to 1.0 with 0 being highest priority and 10 lowest
+    # Race.Zerg: {
+    #     UnitID.HYDRALISK: {"proportion": 0.15, "priority": 0},
+    #     UnitID.ROACH: {"proportion": 0.8, "priority": 1},
+    #     UnitID.ZERGLING: {"proportion": 0.05, "priority": 2},
+    # },
+}
 
 class MyBot(AresBot):
     def __init__(self, game_step_override: Optional[int] = None):
@@ -18,10 +44,31 @@ class MyBot(AresBot):
     async def on_step(self, iteration: int) -> None:
         await super(MyBot, self).on_step(iteration)
 
-        # Register mining behaviors so that worker returns to mining when idle.
-        self.register_behavior(Mining())
-        # step logic here ...
+        self._macro()
+
         pass
+
+    def _macro(self) -> None:
+        # MINE
+        # ares-sc2 Mining behavior
+        # https://aressc2.github.io/ares-sc2/api_reference/behaviors/macro_behaviors.html#ares.behaviors.macro.mining.Mining
+        self.register_behavior(Mining())
+
+        # MAKE SUPPLY
+        # ares-sc2 AutoSupply
+        # https://aressc2.github.io/ares-sc2/api_reference/behaviors/macro_behaviors.html#ares.behaviors.macro.auto_supply.AutoSupply
+        if self.build_order_runner.build_completed:
+            self.register_behavior(AutoSupply(base_location=self.start_location))
+
+        # BUILD ARMY
+        # ares-sc2 SpawnController
+        # https://aressc2.github.io/ares-sc2/api_reference/behaviors/macro_behaviors.html#ares.behaviors.macro.spawn_controller.SpawnController
+
+        # production controller
+        self.register_behavior(
+            ProductionController(ARMY_COMPS[self.race], self.start_location)
+        )
+        self.register_behavior(SpawnController(ARMY_COMPS[self.race]))
 
     """
     Can use `python-sc2` hooks as usual, but make a call the inherited method in the superclass
